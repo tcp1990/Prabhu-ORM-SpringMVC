4de5d49a04d7050d80769ca14661408ad3fcc6f9
prabhu <prabhu_tc@yahoo.com>
2022-09-27 19:14:54 +0530

    [updating Customer.java]

--- src/main/java/com/gl/crm/model/Customer.java
+++ src/main/java/com/gl/crm/model/Customer.java
@@ -1,5 +1,65 @@
 package com.gl.crm.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String eMail;
+
+	public Customer() {
+		super();
+	}
+
+	public Customer(String firstName, String lastName, String eMail) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.eMail = eMail;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", eMail=" + eMail + "]";
+	}
 
 }
