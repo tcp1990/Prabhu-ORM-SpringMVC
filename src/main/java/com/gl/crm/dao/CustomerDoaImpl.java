d195135f7d6e089fd1a29cd810370e805d2a5a92
prabhu <prabhu_tc@yahoo.com>
2022-09-27 19:27:58 +0530

    [Updating CustomerDoaImpl.java]

--- src/main/java/com/gl/crm/dao/CustomerDoaImpl.java
+++ src/main/java/com/gl/crm/dao/CustomerDoaImpl.java
@@ -1,5 +1,69 @@
 package com.gl.crm.dao;
 
-public class CustomerDoaImpl {
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gl.crm.model.Customer;
+
+@Repository
+public class CustomerDoaImpl implements CustomerDoa {
+
+	@Autowired
+	SessionFactory factory;
+
+	@Override
+	@Transactional
+	public void saveCustomer(Customer customer) {
+		Session session = factory.getCurrentSession();
+		session.saveOrUpdate(customer);
+	}
+
+	@Override
+	@Transactional
+	public Customer findById(int id) {
+		Session session = factory.getCurrentSession();
+		Customer cust = session.get(Customer.class, id);
+		System.out.println(cust);
+		return cust;
+	}
+
+	@Override
+	@Transactional
+	public List<Customer> findAll() {
+		Session session = factory.getCurrentSession();
+		Query query = session.createQuery("from Customer");
+		return query.getResultList();
+	}
+
+	@Override
+	@Transactional
+	public void deleteCustomer(int id) {
+		Session session = factory.getCurrentSession();
+		Customer cust = session.load(Customer.class, id);
+		session.delete(cust);
+
+	}
+
+	@Override
+	@Transactional
+	public List<Customer> search(String name, String department) {
+		Session session = factory.getCurrentSession();
+		Criteria c = session.createCriteria(Customer.class);
+		Criterion n = Restrictions.eq("name", name);
+		Criterion d = Restrictions.eq("department", department);
+		Criterion combine = Restrictions.and(n, d);
+		c.add(combine);
+		return c.list();
+	}
 
 }
