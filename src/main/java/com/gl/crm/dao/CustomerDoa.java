fda6aaedb60dc45d4be1ecd1cb6a0835f0cfbfb2
prabhu <prabhu_tc@yahoo.com>
2022-09-27 19:20:02 +0530

    [Updating CustomerDoa.java]

--- src/main/java/com/gl/crm/dao/CustomerDoa.java
+++ src/main/java/com/gl/crm/dao/CustomerDoa.java
@@ -1,5 +1,18 @@
 package com.gl.crm.dao;
 
-public class CustomerDoa {
+import java.util.List;
 
+import com.gl.crm.model.Customer;
+
+public interface CustomerDoa {
+
+	public void saveCustomer(Customer customer);
+
+	public Customer findById(int id);
+
+	public List<Customer> findAll();
+
+	public void deleteCustomer(int id);
+
+	public List<Customer> search(String firstName, String lastName);
 }
